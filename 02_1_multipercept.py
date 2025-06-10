ded9941fb41c764314294b27c587b2000b24b9d2
Noksek2 <banwonkim@naver.com>
2025-06-11 00:18:09 +0900

    Add files via upload

---
+++ 02_1_multipercept.py
@@ -0,0 +1,24 @@
+import numpy as np
+def Percept(w1,w2,theta):
+    W=np.array([[w1],[w2]])
+    B=np.array([-theta])
+    def H(x1,x2):
+        X=np.array([x1,x2])
+        return np.sum(np.dot(X,W)+B)>0
+    return H
+def PrintPercept(pf,str):
+    for i in range(0,4):
+        x1,x2=i//2,i%2 
+        print(f'{str} {x1},{x2} = {pf(x1,x2)}')
+        
+Pand=Percept(0.5,0.5,0.9)
+Por =Percept(0.5,0.5,0.4)
+Pnand=Percept(-0.5,-0.5,-0.9)
+
+PrintPercept(Pand,'AND')
+PrintPercept(Por,'OR')
+PrintPercept(Pnand,'NAND')
+
+Pxor=lambda x1,x2:Pand(Por(x1,x2),Pnand(x1,x2))
+
+PrintPercept(Pxor,'XOR')
